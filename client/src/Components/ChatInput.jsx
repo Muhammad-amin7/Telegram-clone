24a9ab74c18028a7e40976c240a0547c5d99ba2b
Maqsud G <maqsudmuhammadiyev65@gmail.com>
2025-04-25 22:21:46 +0500

    Refactor: Replace send button icon with text in ChatInput component

--- client/src/Components/ChatInput.jsx
+++ client/src/Components/ChatInput.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { BsSend } from "react-icons/bs";
 import { useSendChat } from '../hooks/useSendChat';
 
 export default function ChatInput({ id }) {
@@ -18,7 +17,7 @@ export default function ChatInput({ id }) {
             <section className='absolute bottom-0 left-0 w-[100%] flex items-center justify-between '>
                   <form onSubmit={handleSend} className="relative w-[100%]">
                         <input name='message' type="text" className='rounded-2xl px-2.5 py-1 w-[100%] outline-black bg-zinc-900' />
-                        <button className='absolute top-1/2 transform -translate-y-1/2 right-4'><BsSend /></button>
+                        <button className='absolute top-1/2 transform -translate-y-1/2 right-4'>Send</button>
                   </form>
             </section>
       )
