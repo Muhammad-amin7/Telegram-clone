530033028f1f853ddd0bc950a9b6d214bf3a57e5
Maqsud G <maqsudmuhammadiyev65@gmail.com>
2025-04-28 09:45:41 +0500

    Yozsa ketvotti joyida Text

--- client/src/Components/LeftSideBar.jsx
+++ client/src/Components/LeftSideBar.jsx
@@ -13,7 +13,7 @@ export default function LeftSideBar({ sampleChatData }) {
 
   useEffect(() => {
     sendID(activeChatId);
-  }, [activeChatId])
+  }, [activeChatId, data])
 
   return (
     <div className="flex h-screen">
